71a2fe72984e794c9a21d547be9031396ca0934a
user <user@user92>
2017-10-31 16:31:55 +0900

    공지사항 완성

---
+++ src/main/java/com/gamsa/webapp/dao/QnaDao.java
@@ -0,0 +1,19 @@
+package com.gamsa.webapp.dao;
+
+import java.util.List;
+
+import com.gamsa.webapp.entity.Notice;
+import com.gamsa.webapp.entity.Qna;
+
+public interface QnaDao {
+
+	Qna get(String id); //mybatis에 종속시켜도 될때는 인터페이스를 바꿔도 된다... 하지만 인터페이스는 다른데서도 사용가능하도록 안건드리는게,...	
+	Notice delete(String id);
+	int update(String id, String title, String content);
+	Qna getPrev(String id);
+	Qna getNext(String id);
+	String getNextId();
+	List<Qna> getList(int page, String field, String query);
+	int insert(String title, String content, String writerId);
+	int insert(Qna qna);
+}
--- src/main/java/com/gamsa/webapp/entity/Qna.java
+++ src/main/java/com/gamsa/webapp/entity/Qna.java
@@ -1,72 +1,88 @@
 package com.gamsa.webapp.entity;
-
 import java.util.Date;
 
 public class Qna {
-	
-	private int id;
-	private String title;
-	private String content;
-	private Date regDate;
-	private String writerId;
-	
-	public Qna() {
-		// TODO Auto-generated constructor stub
-	}
-	
-	
+   private String id;
+   private String title;
+   private String content;
+   private Date regDate;
+   private String writerId;
+   private int hit;
 
-	public Qna(int id, String title, String content, Date regDate, String writerId) {
+   public Qna() {
+	   // TODO Auto-generated constructor stub
+   }
+
+   
+
+	public Qna(String id, String title, String content, Date regDate, String writerId, int hit) {
 		super();
 		this.id = id;
 		this.title = title;
 		this.content = content;
 		this.regDate = regDate;
 		this.writerId = writerId;
+		this.hit = hit;
+	}
+
+
+	public Qna(String title, String content, String writerId) {
+		super();
+		this.title = title;
+		this.content = content;
+		this.writerId = writerId;
 	}
 
 
 
-	public int getId() {
+	public String getId() {
 		return id;
 	}
-
-	public void setId(int id) {
+	
+	public void setId(String id) {
 		this.id = id;
 	}
-
+	
 	public String getTitle() {
 		return title;
 	}
-
+	
 	public void setTitle(String title) {
 		this.title = title;
 	}
-
+	
 	public String getContent() {
 		return content;
 	}
-
+	
 	public void setContent(String content) {
 		this.content = content;
 	}
-
+	
 	public Date getRegDate() {
 		return regDate;
 	}
-
+	
 	public void setRegDate(Date regDate) {
 		this.regDate = regDate;
 	}
-
+	
 	public String getWriterId() {
 		return writerId;
 	}
-
+	
 	public void setWriterId(String writerId) {
 		this.writerId = writerId;
 	}
 	
+	public int getHit() {
+		return hit;
+	}
 	
-	
+	public void setHit(int hit) {
+		this.hit = hit;
+	}
+	   
+	   
+   
 }
